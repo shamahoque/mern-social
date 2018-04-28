f31bb6660dd686d006dca6b2454d828eb630de7f
Shama Hoque <shama.hoque@gmail.com>
2018-04-29 01:42:49 +0600

    production ready

--- server/express.js
+++ server/express.js
@@ -23,13 +23,13 @@ import {teal, orange} from 'material-ui/colors'
 //end
 
 //comment out before building for production
-import devBundle from './devBundle'
+//import devBundle from './devBundle'
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
 
 //comment out before building for production
-devBundle.compile(app)
+//devBundle.compile(app)
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
