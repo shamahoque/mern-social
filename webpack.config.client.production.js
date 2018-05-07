3f1b458ffd916c6d0b82cc45460aae410b5d4fd9
Shama Hoque <shama.hoque@gmail.com>
2018-05-07 22:28:43 +0600

    cleanup

--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
+
 const config = {
     mode: "production",
     entry: [
@@ -27,4 +28,5 @@ const config = {
         ]
     }
 }
+
 module.exports = config
